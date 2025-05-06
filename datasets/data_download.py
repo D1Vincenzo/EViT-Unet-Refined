41f1f9622bd107ad05c44c6ea782126191e1a581
Xinyang Huang <huangxinyang030218@126.com>
2025-05-06 15:04:48 +0100

    update data downloader

--- datasets/data_download.py
+++ datasets/data_download.py
@@ -4,4 +4,10 @@ from download_token import token
 
 syn = synapseclient.Synapse() 
 syn.login(authToken=token) 
-files = synapseutils.syncFromSynapse(syn, 'syn3193805', path='datasets/synapse_data', downloadFile=True)
+# files = synapseutils.syncFromSynapse(syn, 'syn3379050', path='datasets/synapse_data', downloadFile=True)
+
+# Obtain a pointer and download the data
+syn3379050 = syn.get(entity='syn3379050', version=1 )
+
+# Get the path to the local copy of the data file
+filepath = syn3379050.path
